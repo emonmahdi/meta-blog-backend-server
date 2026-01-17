0cb10feb51d4b4f0993beb043872ccdef7d1f809
emonmahdi <developeremon1997@gmail.com>
2026-01-17 12:13:03 +0600

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-purple-300 text-2xl p-4 text-secondary">
+        Meta Blog app
+      </h2>
+    </>
+  );
+}
+
+export default App;
