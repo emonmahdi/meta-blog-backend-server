b3d62c030e7a60b7c1691b639b1d3b43dcb0b3ba
emonmahdi <developeremon1997@gmail.com>
2026-01-20 15:35:35 +0600

    get single blog api done

--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -25,6 +25,7 @@ router.post("/add-blog", async (req, res) => {
   }
 });
 
+// Get all Blogs APIs
 router.get("/", async (req, res) => {
   try {
     const result = await Blog.find().sort({ createdAt: -1 });
@@ -39,6 +40,33 @@ router.get("/", async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Something Error! Post Not Fetched",
+      error: err.message,
+    });
+  }
+});
+// Get Single Blog APIs
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Blog.findById(id);
+
+    if (!result) {
+      res.status(500).json({
+        success: false,
+        message: "Blog not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Blog Fetched Successfully",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Something Error! Blog Not Fetched",
       error: err.message,
     });
   }
