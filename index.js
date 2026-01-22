6497e4c17a961e92e30a905536f7f1f753f63ab1
emonmahdi <developeremon1997@gmail.com>
2026-01-22 16:07:11 +0600

    udpate cors link origin url

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-  }),
+    origin: [
+      "http://localhost:5173",
+      "https://meta-blog-frontend-rouge.vercel.app",
+    ],
+  })
 );
 
 //Use Routes
